3c553d5c275d90b827ee4942a798f227cb8371e3
Galen Strasen <galen@wearetmbr.com>
2016-04-09 16:16:44 -0700

    Default page markup

--- wp-content/themes/tmbr/page.php
+++ wp-content/themes/tmbr/page.php
@@ -1,17 +1,22 @@
-<?php get_header(); ?>
-<div class="pg-content">	
-<div class="container">
-	<div class="row">
-	<div class="span12">
-	<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-					
-				<h1><?php the_title(); ?></h1>		
-		
-	<?php the_content(); ?>
-	</div>
-	</div>
-	<?php endwhile; ?>
-</div>
-</div>
+<?php get_header();
 
+ if ( have_posts() ) : while ( have_posts() ) : the_post();
+
+?>
+<div class="pg-content">
+	<div class="container">
+		<div class="row">
+			<div class="span12">
+				<h1><?php the_title(); ?></h1>
+				<?php
+					get_template_part( 'partials/header-content' );
+					the_content();
+				?>
+
+			</div>
+		</div>
+	</div>
+	<?php get_template_part( 'partials/page-content' ); ?>
+</div>
+<?php endwhile; endif; ?>
 <?php get_footer(); ?>
