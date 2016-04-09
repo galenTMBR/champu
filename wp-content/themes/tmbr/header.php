86f0e2564511fe36b18065dcd8da4604576841eb
Galen Strasen <galen@wearetmbr.com>
2016-04-09 16:35:57 -0700

    Tied in global fields

--- wp-content/themes/tmbr/header.php
+++ wp-content/themes/tmbr/header.php
@@ -22,21 +22,21 @@
 
 	<!-- >> THEME STYLESHEET -->
 	<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" />
-	
+
 	<!-- >> RESPONSIVE STYLES -->
     <link rel="stylesheet" href="<?php bloginfo( 'template_url' ); ?>/css/responsive.css">
-  
-		
+
+
 	<!-- >> LOAD JQUERY -->
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
 	<script>window.jQuery || document.write('<script src="js/jquery-1.8.1.min.js"><\/script>')</script> <!-- Pull jQuery locally if Google Fails -->
-	
+
 	<!-- >> PINGBACK -->
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
-	
-	
-	
+
+
+
 	<?php wp_head(); ?>
 </head>
 
@@ -47,32 +47,40 @@
 		<div class="container">
 			<div class="row">
 				<div class="span12">
-				<h5><a href="/contact/" >&#126; Call us at <span>307.739.4646</span> to book an appointment today. &#126;</a></h5>
+				<h5><?php the_field('header_callout_text','option'); ?></h5>
 				</div>
 			</div>
 		</div>
 	</div>
 	<!-- BEGIN HEADER -->
 	<div id="header">
-	
+
 		<div class="container">
 			<div class="row">
-			
+				<?php
+				$logo = get_field('global_logo','option');
+				if($logo) {
+					$logo_img = $logo['sizes']['medium'];
+				}
+				else {
+					$logo_img = '/wp-content/themes/tmbr/img/champu_logo.png';
+				}
+				?>
 				<div class="span5">
-					<a href="/home/" class="logo">
-					<img src="<?php bloginfo( 'template_url' ); ?>/img/champu_logo.png" width="250" alt="Champu Salon - Jackson Hole" />
+					<a href="/" class="logo">
+					<img src="<?php echo $logo_img; ?>" width="250" alt="Champu Salon - Jackson Hole" />
 					</a>
 				</div><!-- /span5 -->
-				
+
 				<div class="span7">
 					<?php wp_nav_menu( array( 'theme_location' => 'main-navigation', 'container' => 'nav', 'menu_class' => 'nav' ) ); ?>
 				</div><!-- /span7 -->
-				
+
 			</div><!-- /row  -->
 		</div><!-- /container -->
-		
+
 	</div><!-- /#HEADER -->
-	
+
 
 
 
