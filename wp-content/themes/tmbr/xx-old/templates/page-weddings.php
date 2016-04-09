eeb4a48a44d3c572bcab2d875e87214a6e620cb3
Galen Strasen <galen@wearetmbr.com>
2016-04-08 18:37:19 -0700

    Old template - get outta here

--- wp-content/themes/tmbr/xx-old/templates/page-weddings.php
+++ wp-content/themes/tmbr/xx-old/templates/page-weddings.php
@@ -2,23 +2,28 @@
 /*
 
 Template Name: Weddings
-
+$wedding_gallery
 */ ?>
 
 
 <?php get_header(); ?>
 <div class="pg-content">
 <div class="container">
+<div class="row">
+      <div class="span12">
 	<!-- SLIDESHOW -->
 	<h1>Weddings</h1>
+</div></div>
+	<?php get_template_part( 'partials/header-content' );
+?><!--
 	<div class="flexslider">
 	  	<ul class="slides">
 	  		<li><img src="/wp-content/themes/tmbr/img/slides/wedding-stylist-jackson-hole1.jpg" alt="Jackson Hole Wedding Stylist"/></li>
 	    	<li><img src="/wp-content/themes/tmbr/img/slides/wedding-stylist-jackson-hole2.jpg" alt="Jackson Wyoming Wedding Stylist"/></li>
 			<li><img src="/wp-content/themes/tmbr/img/slides/wedding-stylist-jackson-hole3.jpg" alt="Jackson Hole Wedding Hair Stylist"/></li>
-	    	<li><img src="/wp-content/themes/tmbr/img/slides/wedding-stylist-jackson-hole4.jpg" alt="Jackson Hole Wedding Makeup Stylist"/></li>	    	
+	    	<li><img src="/wp-content/themes/tmbr/img/slides/wedding-stylist-jackson-hole4.jpg" alt="Jackson Hole Wedding Makeup Stylist"/></li>
 	    	<li><img src="/wp-content/themes/tmbr/img/slides/wedding-stylist-jackson-hole5.jpg" alt="Jackson Hole Wyoming Wedding Stylist"/></li>
-	    	<li><img src="/wp-content/themes/tmbr/img/slides/wedding-stylist-jackson-hole6.jpg" alt="Jackson Hole Wyoming Wedding Hair Stylist"/></li>			
+	    	<li><img src="/wp-content/themes/tmbr/img/slides/wedding-stylist-jackson-hole6.jpg" alt="Jackson Hole Wyoming Wedding Hair Stylist"/></li>
 	    	<li><img src="/wp-content/themes/tmbr/img/slides/wedding-stylist-jackson-hole7.jpg" alt="Jackson Hole Wyoming Wedding Makeup Stylist"/></li>
 	    	<li><img src="/wp-content/themes/tmbr/img/slides/wedding5.jpg" alt="Wedding Stylist in Jackson Hole"/></li>
 	    	<li><img src="/wp-content/themes/tmbr/img/slides/wedding7.jpg" alt="Wedding Stylist in Jackson Hole, Wyoming"/></li>
@@ -28,9 +33,9 @@ Template Name: Weddings
 	    	<li><img src="/wp-content/themes/tmbr/img/slides/wedding3.jpg" alt="Wedding Stylist in Jackson Wyoming"/></li>
 	    	<li><img src="/wp-content/themes/tmbr/img/slides/wedding4.jpg" alt="Wedding Stylist in Jackson Hole Wyoming"/></li>
 		</ul>
-	</div><!-- /flexslider -->
+	</div> --><!-- /flexslider -->
 	<p>Our stylists have extensive experience creating the perfect hair and makeup look for you and your bridal party on your wedding day. A personal consultation and hair trial can be added to your package so you can be at ease on the big day, knowing you'll be styled just the way you want. We offer both on-site and in salon services to meet your needs. Feel free to check out our <a href="https://www.facebook.com/ChampuSalon" target="_blank">Facebook</a> and <a href="http://www.pinterest.com/ChampuSalonJH/" target="_blank">Pinterest</a> pages to get a feel for some of the styles we love. Pricing available upon request. Please give us a call at 307-739-4646 to check availability.</p>
 </div>
 </div>
-	
+
 <?php get_footer(); ?>
